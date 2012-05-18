48a028c0810dca81234041b8d4fdb0f7d8e5a098
daler <dalerr@niddk.nih.gov>
2012-05-18 14:05:35 -0400

    use chr1 and 2 instead of 19 for examples

--- metaseq/test/examples/ctcf_peaks_settings.py
+++ metaseq/test/examples/ctcf_peaks_settings.py
@@ -9,7 +9,7 @@ DOWNSTREAM = 1000
 BINS = 100
 FRAGMENT_SIZE = 200
 GENOME = 'hg19'
-CHROMS = ['chr19']
+CHROMS = ['chr1', 'chr2']
 
 gtfdb = metaseq.example_filename('Homo_sapiens.GRCh37.66.cleaned.gtf.db')
 G = gffutils.FeatureDB(gtfdb)
