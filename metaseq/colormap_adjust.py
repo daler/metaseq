ceee1c1c6422d680b2c9d27039d96e298b143b46
daler <dalerr@niddk.nih.gov>
2012-05-18 14:04:18 -0400

    add smart_colormap func for matching the s,v of positive colormap hsv

--- metaseq/colormap_adjust.py
+++ metaseq/colormap_adjust.py
@@ -1,4 +1,6 @@
-# from https://sites.google.com/site/theodoregoetz/notes/matplotlib_colormapadjust
+# cmap_discretize, cmap_powerlaw_adjust, cmap_center_adjust, and
+# cmap_center_adjust are from
+# https://sites.google.com/site/theodoregoetz/notes/matplotlib_colormapadjust
 
 import math, copy
 import numpy
@@ -6,6 +8,61 @@ import numpy as np
 from matplotlib import pyplot, colors, cm
 import matplotlib
 from scipy import interpolate
+import colorsys
+
+def color_test(color):
+    """
+    Figure filled in with `color`; useful for troubleshooting or experimenting
+    with colors
+    """
+    if isinstance(color, np.ndarray):
+        color = color.ravel()
+    fig = pyplot.figure(figsize=(2,2), facecolor=color)
+
+
+def smart_colormap(vmin, vmax, color_high='#b11902', hue_low=0.6):
+    """
+    Creates a "smart" colormap that is centered on zero, and accounts for
+    asymmetrical vmin and vmax by matching saturation/value of high and low
+    colors.
+
+    It works by first creating a colormap from white to `color_high`.  Setting
+    this color to the max(abs([vmin, vmax])), it then determines what the color
+    of min(abs([vmin, vmax])) should be on that scale.  Then it shifts the
+    color to the new hue `hue_low`, and finally creates a new colormap with the
+    new hue-shifted as the low, `color_high` as the max, and centered on zero.
+
+    :param color_high: a matplotlib color -- try "#b11902" for a nice red
+    :param hue_low: float in [0,1] -- try 0.6 for a nice blue
+    :param vmin: lowest value in data you'll be plotting
+    :param vmax: highest value in data you'll be plotting
+    """
+    # first go from white to color_high
+    orig_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
+            'test', ['#FFFFFF', color_high], N=256)
+
+    # For example, say vmin=-3 and vmax=9.  If vmin were positive, what would
+    # its color be?
+    vmin = float(vmin)
+    vmax = float(vmax)
+    mx = max([vmin, vmax])
+    mn = min([vmin, vmax])
+    frac = abs(mn / mx)
+    rgb = orig_cmap(frac)[:-1]
+
+    # Convert to HSV and shift the hue
+    hsv = list(colorsys.rgb_to_hsv(*rgb))
+    hsv[0] = hue_low
+    new_rgb = colorsys.hsv_to_rgb(*hsv)
+    new_hex = matplotlib.colors.rgb2hex(new_rgb)
+
+    # Create a new colormap using the new hue-shifted color as the low end
+    new_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
+            'test', [new_rgb, '#FFFFFF', color_high], N=256)
+
+    # Re-center on zero
+    return cmap_center_point_adjust(new_cmap, [vmin, vmax], 0)
+
 
 def cmap_discretize(cmap, N):
     """Return a discrete colormap from the continuous colormap cmap.
