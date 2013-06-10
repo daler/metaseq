fef30e00b2123ee9acbbee916fc87f11936b964d
daler <dalerr@niddk.nih.gov>
2013-06-10 19:23:51 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,14 @@
 import ez_setup
 ez_setup.use_setuptools()
 from setuptools import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
 import sys
 import os
 import numpy
 
-if 'setuptools.extension' in sys.modules:
-    m = sys.modules['setuptools.extension']
-    m.Extension.__dict__ = m._Extension.__dict__
 
 version_py = os.path.join(os.path.dirname(__file__), 'metaseq', 'version.py')
 version = open(version_py).read().split('=')[-1].strip().replace('"','')
 
-exts = []
-[Extension(
-            'metaseq.rebin',
-            ['metaseq/rebin.pyx'],
-            include_dirs=[numpy.get_include()])]
 
 long_description = """
 metaseq is a Python framework for genomic data analysis (primarily
@@ -29,13 +19,16 @@ HTSeq, gffutils, and matplotlib.
 setup(
         name='metaseq',
         version=version,
-        cmdclass = {'build_ext': build_ext},
         long_description=long_description,
-        ext_modules=exts,
-        install_requires=['bx-python', 'numpy', 'HTSeq', 'matplotlib', 'scipy', 'scikits.learn', 'pybedtools', 'gffutils'],
-        packages=['metaseq', 'metaseq.test', 'metaseq.test.data'],
+        install_requires=['bx-python', 'numpy', 'HTSeq', 'matplotlib', 'scipy',
+                          'scikits.learn', 'pybedtools', 'gffutils',
+                          'argparse'],
+        packages=['metaseq', 'metaseq.test', 'metaseq.test.data',],
         package_data={'metaseq':['test/data/*']},
-        scripts=['metaseq/scripts/download_metaseq_example_data.py'],
+        scripts=[
+            'metaseq/scripts/download_metaseq_example_data.py',
+            'metaseq/scripts/metaseq-cli',
+        ],
         author='Ryan Dale',
         author_email='dalerr@niddk.nih.gov',
         url='http://github.com/daler/metaseq',
