50411f3002d4fb197646d7761b0cb812eae4cdb6
daler <dalerr@niddk.nih.gov>
2013-08-06 12:46:55 -0400

    more __init__ imports; make genomic_signal module accessible

--- metaseq/__init__.py
+++ metaseq/__init__.py
@@ -1,10 +1,13 @@
 import os
 import sys
 import time
+import helpers
 from helpers import data_dir, example_filename, \
     gfffeature_to_interval
+import genomic_signal as _genomic_signal
 from genomic_signal import genomic_signal
 import plotutils
 import integration
 import colormap_adjust
+import results_table
 from version import __version__
