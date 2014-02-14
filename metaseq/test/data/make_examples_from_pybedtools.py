8b1eefd902130227401cca3b1583d75e21e70d6a
daler <dalerr@niddk.nih.gov>
2014-02-14 18:02:57 -0500

    make smaller bam file for testing

--- metaseq/test/data/make_examples_from_pybedtools.py
+++ metaseq/test/data/make_examples_from_pybedtools.py
@@ -9,21 +9,28 @@ import metaseq
 
 # new bam file without the read names
 x = pysam.Samfile(pybedtools.example_filename('x.bam'))
-s = pysam.Samfile('x.bam', template=x, mode='wb')
+s = pysam.Samfile('x.bam.tmp', template=x, mode='wb')
 for i, r in enumerate(x):
     r.qname = str(i)
     r.cigarstring = '36M'
     s.write(r)
 s.close()
 
-os.system('samtools index x.bam')
-bam = pybedtools.BedTool('x.bam')
-
+bam = pybedtools.BedTool('x.bam.tmp')
 pybedtools.contrib.bigwig.bam_to_bigwig(
     bam.fn, 'dm3', 'x.bigwig', scale=False)
 
 bed = bam.bam_to_bed(output='x.bed', split=True)
 pybedtools.contrib.bigbed.bigbed(bed.sort(), 'dm3', 'x.bigbed')
-
 bed.tabix(in_place=True)
 os.unlink(bed.fn)
+
+os.system(' '.join([
+    'samtools view -h x.bam | awk -F "\t"',
+    '\'{OFS="\t"; if (substr($1, 1, 1) == "@") print $0; else print $1,$2,$3,$4,$5,$6,$7,$8,$9,"*", "*",$12}\'',
+    '| samtools view -Sb - > x2.bam'])
+)
+
+os.system('mv x2.bam x.bam')
+os.system('samtools index x.bam')
+os.system('rm x.bam.tmp')
