de1f2dcf1b45e98785a7d5a74cf47649e02462e5
daler <dalerr@niddk.nih.gov>
2014-03-29 13:59:24 -0400

    improve speedtest benchmarking

--- metaseq/scripts/speedtest.py
+++ metaseq/scripts/speedtest.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 usage="""
-Benchmarking script to decide how many processors are optimal to use.
+This is a benchmarking script for the metaseq Python package
+(https://github.com/daler/metaseq).  It can help decide how many processors are
+optimal to use for your particular hardware configuration.
 
-This will probably be a function of CPU speed and disk I/O throughput, so it's
-best to run on each new hardware configuration.
-
-Note that the actual time per 1k features on your own data will also be
-a function of data density per feature (for BAM/BED/bigBed).
+The default arguments work for data shipped along with metaseq.  The default
+arguments generate 1000 random features of about 5kb in size and then extract
+signal data for BED, BAM, bigWig, and bigBed formats. This is done using from
+1 to however many CPUs are availble.
 
 The result is a plot showing the actual time in seconds per 1000 features, over
 a range of processors.  A dotted line for the optimal 1/x speedup.  The plot is
 also saved as a PDF.
 
-The default arguments work for data shipped with metaseq.
+Note that results are a function of CPU speed, disk I/O throughput, and nature
+of the data (for BAM, BED, and bigBed formats), so your mileage may vary.  For
+example, multiple CPUs will not help if there's only a small number of
+features.
 """
 import sys
 import numpy as np
@@ -27,24 +31,39 @@ import os
 ap = argparse.ArgumentParser(usage=usage)
 ap.add_argument(
     '-n', '--nfeatures', type=int, default=1000,
-    help='Number of random windows to make')
+    help='Number of random windows to make.  The size of each feature is '
+    '(start - stop) / nfeatures, so changing this parameter will affect '
+    'the size of each feature.')
 ap.add_argument(
     '-c', '--chunksize', type=int, default=100,
     help='Number of windows to send to each process')
 ap.add_argument(
     '--start', type=int, default=10000,
-    help='start coord of possible windows')
+    help='start coord of possible windows. See --nfeatures help regarding size.')
 ap.add_argument(
     '--stop', type=int, default=5000000,
-    help='stop coord of possible windows')
-ap.add_argument('--chrom', default='chr2L', help='chromsome to make windows on')
-ap.add_argument('--type', default='all', 
-                help='Only use the specified file types.  Either "all" (default) or a comma-separated list of [bam, bigwig, bed, bigbed]')
-ap.add_argument('--prefix', default=os.path.join(metaseq.data_dir(), 'x'), help='Prefix of filenames to use.  Expects files '
-                'with this prefix, and the following suffixes: .bam, .bigwig, '
-                '.bed.gz (should already be tabixed), .bigbed.  Default: %(default)s')
-ap.add_argument('--plot-prefix', default='speedtest', help='Filename used to save the resulting plot. Default is %(default)s')
+    help='stop coord of possible windows. See --nfeatures help regarding size.')
+ap.add_argument(
+    '--chrom', default='chr2L',
+    help='chromsome to make windows on')
+ap.add_argument(
+    '--type', default='all',
+    help='Only use the specified file types.  Either "all" '
+    '(default) or a comma-separated list of [bam, bigwig, bed, '
+    'bigbed].')
+ap.add_argument(
+    '--prefix', default=os.path.join(metaseq.data_dir(), 'x'),
+    help='Prefix of filenames to use.  Expects files '
+    'with this prefix, and the following suffixes: .bam, .bigwig, '
+    '.bed.gz (should already be tabixed), .bigbed.  Default: %(default)s')
+ap.add_argument(
+    '--plot-prefix', default='./speedtest',
+    help='Filename used to save the resulting plot. Default is %(default)s')
+ap.add_argument(
+    '--bins', default=100,
+    help='Number of bins for each feature')
 args = ap.parse_args()
+
 
 requested = args.type.split(',')
 allowed = ['bam', 'bed', 'bigwig', 'bigbed', 'all']
@@ -59,9 +78,6 @@ intervals = pybedtools.BedTool().window_maker(
     .shuffle(genome={args.chrom: (args.start, args.stop)}, seed=1)
 
 size = (args.stop - args.start) / args.nfeatures
-print size
-print args.type
-
 if args.type == 'all':
     requested = ['bigwig', 'bam', 'bigbed', 'bed']
 signals = []
@@ -71,20 +87,45 @@ for req in requested:
     else:
         signals.append(metaseq.genomic_signal(args.prefix + '.bed.gz', 'bed'))
 
+files = '* ' + '\n  * '.join([i.fn for i in signals])
+plot_filename = (args.plot_prefix + '-%s_features-%s_bp_chunksize=%s.pdf'
+                 % (args.nfeatures, size, args.chunksize))
+processes = range(1, multiprocessing.cpu_count() + 1)
+max_proc = processes[-1]
+print """
+{usage}
+Parameters
+----------
+This script will generate {args.nfeatures} random features, each about {size}
+bp, from genomic coordinates {args.chrom}:{args.start}-{args.stop}.
+
+Genomic signal will be extracted from these files:
+  {files}
+
+For each file, the signal for the {args.nfeatures} features will be extracted
+in parallel and binned into {args.bins} bins, using from 1 to {max_proc} CPUs.
+Each CPU will get as many as {args.chunksize} features at a time.
+
+Plot will be saved as {plot_filename}.
+""".format(**locals())
+
+
+
 d = {}
 results = {}
-processes = range(1, multiprocessing.cpu_count() + 1)
 for x in signals:
-    print x.kind
+    print '\n' + x.kind, '[%s]' % x.fn
     sys.stdout.flush()
     times = []
+    print 'CPUs:',
     for p in processes:
-        print p, 'processes'
+        print p,
+        sys.stdout.flush()
         if p == 1:
             p = None
         t = []
         t0 = time.time()
-        a = x.array(intervals, bins=100, processes=p,
+        a = x.array(intervals, bins=args.bins, processes=p,
                     chunksize=args.chunksize)
         elapsed = time.time() - t0
         times.append(elapsed)
@@ -109,6 +150,5 @@ for k, t in d.items():
     i += 1
 fig.subplots_adjust(left=0.1, right=0.95, wspace=0.5, hspace=0.5)
 fig.suptitle('%s features, %sbp each, chunksize=%s' % (args.nfeatures, size, args.chunksize))
-fig.savefig(
-    'speedtest-%s_features-%s_bp_chunksize=%s.pdf' % (args.nfeatures, size, args.chunksize))
+fig.savefig(plot_filename)
 plt.show()
